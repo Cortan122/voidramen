f4287fde4af2c407d6da8553aae4b649bc7419f7
Cortan122 <Cortan122@gmail.com>
2022-09-16 12:50:50 +0300

    yt-range: audio mode

--- .local/bin/youtube-dl-range.sh
+++ .local/bin/youtube-dl-range.sh
@@ -2,7 +2,10 @@
 
 [ "$1" == "--help" ] && cat -- "$0" && exit
 # Usage:
-# ./youtube-dl-range.sh URL [[STRAT_TIME] STRAT_TIME_2] DURATION
+# ./youtube-dl-range.sh [--audio] URL [[STRAT_TIME] STRAT_TIME_2] DURATION
+
+audio=false
+[ "$1" == "--audio" ] && audio=true && shift
 
 temp="$(mktemp)"
 youtube-dl --get-url --get-filename "$1" > "$temp" # THIS IS STUPID
@@ -10,11 +13,20 @@ readarray -t urls < "$temp"
 rm "$temp"
 # urls[2] = filename
 
-if [ -z "$3" ]; then
-  ffmpeg -i "${urls[0]}" -i "${urls[1]}" -t "$2" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
-elif ! [ -z "$4" ]; then
-  ffmpeg -ss "$2" -i "${urls[0]}" -ss "$2" -i "${urls[1]}" -ss "$3" -t "$4" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
+if $audio; then
+  if [ -z "$3" ]; then
+    ffmpeg -i "${urls[1]}" -t "$2" -map 0:a -c copy "${urls[2]%.*}".ogg
+  elif ! [ -z "$4" ]; then
+    ffmpeg -ss "$2" -i "${urls[1]}" -ss "$3" -t "$4" -map 0:a -c copy "${urls[2]%.*}".ogg
+  else
+    ffmpeg -ss "$2" -i "${urls[1]}" -t "$3" -map 0:a -c copy "${urls[2]%.*}".ogg
+  fi
 else
-  ffmpeg -ss "$2" -i "${urls[0]}" -ss "$2" -i "${urls[1]}" -t "$3" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
+  if [ -z "$3" ]; then
+    ffmpeg -i "${urls[0]}" -i "${urls[1]}" -t "$2" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
+  elif ! [ -z "$4" ]; then
+    ffmpeg -ss "$2" -i "${urls[0]}" -ss "$2" -i "${urls[1]}" -ss "$3" -t "$4" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
+  else
+    ffmpeg -ss "$2" -i "${urls[0]}" -ss "$2" -i "${urls[1]}" -t "$3" -map 0:v -map 1:a -c copy "${urls[2]%.*}".mkv
+  fi
 fi
-
