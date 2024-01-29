c06bdf534f1f560793d6fa5745cf9c2dda6e0acf
Cortan122 <Cortan122@gmail.com>
2024-01-29 02:44:52 +0100

    simple script to copy firefox configs (should be part of install_as_user.sh)

---
+++ configure_firefox.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd -- "$(dirname -- "$0")"
+
+profile="$HOME/.mozilla/firefox/$(awk -F= '/Default/ {print $2;exit}' ~/.mozilla/firefox/profiles.ini)/"
+cp -rvu .config/firefox-chrome -T "$profile/chrome"
+
+
