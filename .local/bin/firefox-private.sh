66f9a6b96fe02b3433ac6bb753b91888cf73090b
Cortan122 <Cortan122@gmail.com>
2024-04-19 00:01:29 +0200

    fixed firefox ctrl+shift+n

--- .local/bin/firefox-private.sh
+++ .local/bin/firefox-private.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-window_id="$(xdotool search --onlyvisible --class Firefox | head -n 1)"
+xdotool search --onlyvisible --class Firefox |
+  while read id; do
+    window_id="$id"
+    xdotool getwindowgeometry "$id" | grep -Fq "Geometry: 1x1" || break
+  done
 
 if [ -z "$window_id" ]; then
   firefox &
   exit
 fi
 
-xdotool windowactivate "$window_id" && xdotool key ctrl+shift+p
+xdotool windowactivate "$window_id" key ctrl+shift+p
