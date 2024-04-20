3258cfcb1b27906b890a7aa69e5ebea1e9413502
Cortan122 <Cortan122@gmail.com>
2024-04-20 18:18:15 +0200

    massacred firefox-private.sh script

--- .local/bin/firefox-private.sh
+++ .local/bin/firefox-private.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-xdotool search --onlyvisible --class Firefox |
-  while read id; do
-    window_id="$id"
-    xdotool getwindowgeometry "$id" | grep -Fq "Geometry: 1x1" || break
-  done
-
-if [ -z "$window_id" ]; then
-  firefox &
-  exit
-fi
-
-xdotool windowactivate "$window_id" key ctrl+shift+p
+# it would've been much easier to just call `firefox --private-window`
+# so that's what i'm gonna do...
+# fuck all that old code!
+firefox --private-window &
