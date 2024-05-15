505e3ee8a9f4958da9af22aa53c0eb538d126e63
Cortan122 <Cortan122@gmail.com>
2024-05-15 15:56:02 +0200

    locate history

--- .local/bin/reload-blocks.sh
+++ .local/bin/reload-blocks.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+for i in `seq 1 10`; do
+  grep -q up /sys/class/net/[we]*/operstate && break
+  echo "Waiting for network..."
+  sleep "$i"
+done
+
 pkill --signal SIGRTMIN+5 i3blocks
 pkill --signal SIGRTMIN+7 i3blocks
 pkill --signal SIGRTMIN+10 i3blocks
--- .local/bin/rofi-locate.sh
+++ .local/bin/rofi-locate.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+history="$HOME/.config/locate_history"
+
+[ -f "$history" ] || touch "$history"
+
 if [ "$ROFI_RETV" = 0 ]; then
   # Initial call of script.
+  awk '!x[$0]++' "$history"
   grep -Pi '^locate [^-][^ ]*$' ~/.config/bash_history | cut -d' ' -f2 | sort | uniq
 elif [ -e "$1" ]; then
+  basename -- "$1" >> "$history"
   coproc ( xdg-open "$1" >/dev/null 2>&1 )
 else
-  locate --limit 100 -- "$1"
+  locate --ignore-case --limit 100 -- "$1" || echo "Nothing found..."
 fi
