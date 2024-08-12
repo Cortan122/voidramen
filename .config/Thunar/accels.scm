9c722c38d89a6d8de2051aa9dedc5758cd70376c
Cortan122 <Cortan122@gmail.com>
2024-08-13 01:45:36 +0200

    thunar config

---
+++ .config/Thunar/accels.scm
@@ -0,0 +1,4 @@
+; thunar GtkAccelMap rc-file         -*- scheme -*-
+
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1723502476941361-1" "<Shift><Super>i")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1713469089098780-1" "<Shift><Super>t")
