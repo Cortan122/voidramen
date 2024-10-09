51f5cbccd8d1b94cf8d2ea6edab699cdf198b79f
Cortan122 <Cortan122@gmail.com>
2024-10-09 19:48:23 +0200

    funny volume changes

--- .local/bin/hotkeys/change-volume.sh
+++ .local/bin/hotkeys/change-volume.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+mic_mute () {
+  wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+  volume="$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)"
+  notify-send -t 500 "Mic volume: 🎤 $volume"
+  exit
+}
+
 case $1 in
   mute) BLOCK_BUTTON=2 ;;
   up) BLOCK_BUTTON=4 ;;
   down) BLOCK_BUTTON=5 ;;
+  mic-mute) mic_mute ;;
 esac
 
 export BLOCK_BUTTON
