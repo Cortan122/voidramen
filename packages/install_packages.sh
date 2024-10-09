8792201b428ba023422ef4910fa97df696971eee
Cortan122 <Cortan122@gmail.com>
2024-10-09 14:09:41 +0200

    compare versions with sort(1)

--- packages/install_packages.sh
+++ packages/install_packages.sh
@@ -6,14 +6,21 @@ cd -- "$(dirname -- "$0")"
 list="$(pacman -Q)"
 to_build=()
 
+function version-lte {
+  printf '%s\n' "$1" "$2" | sort -C -V
+}
+
 function check-pkgname {
   name="$(grep -Po '(?<=pkgname=).*' "$1"/PKGBUILD)"
   if ! grep -q "$name" <<<"$list"; then
     to_build+=("$1")
   else
     commit="$(git log -n 1 --pretty=format:%H -- "$1"/PKGBUILD)"
-    rev="r$(git rev-list --count "$commit").$(git rev-parse --short=7 "$commit")"
-    if ! grep -q "$name $rev" <<<"$list"; then
+    new_version="r$(git rev-list --count "$commit").$(git rev-parse --short=7 "$commit")"
+    old_version="$(grep -Po "(?<=$name )"'r[0-9]+\.[a-f0-9]{7}' <<<"$list")"
+
+    if ! version-lte "$new_version" "$old_version"; then
+      echo "$name: $old_version is less than $new_version. reinstalling..."
       to_build+=("$1")
     fi
   fi
