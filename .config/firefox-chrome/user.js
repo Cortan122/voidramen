6e156512f02609dbf6eea9c994105dd6f0812085
Cortan122 <Cortan122@gmail.com>
2024-10-25 18:29:49 +0200

    game shortcuts

--- .config/firefox-chrome/user.js
+++ .config/firefox-chrome/user.js
@@ -14,3 +14,4 @@ user_pref("browser.gesture.swipe.right", "");
 user_pref("widget.non-native-theme.scrollbar.style", 1);
 user_pref("full-screen-api.warning.timeout", 500);
 user_pref("print.print_paper_name", "iso_a4");
+user_pref("browser.toolbars.bookmarks.visibility", "newtab");
