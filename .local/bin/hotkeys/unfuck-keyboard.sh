61e31e28cb9408d54e5e085f314480454a2d7d0d
Cortan122 <Cortan122@gmail.com>
2025-02-06 01:56:41 +0100

    aliases and more keyboards

--- .local/bin/hotkeys/unfuck-keyboard.sh
+++ .local/bin/hotkeys/unfuck-keyboard.sh
@@ -2,6 +2,12 @@
 
 if [ "$1" = "--colemak" ]; then
   setxkbmap us,us -variant ,colemak_dh -option grp:toggle,grp:caps_toggle,grp:alt_shift_toggle,grp_led:caps
+elif [ "$1" = "--normal" ]; then
+  xkbcomp -w2 ~/.config/x11/keymap.xkb "$DISPLAY"
+elif [ "$1" = "--cursed" ]; then
+  xkbcomp -w2 ~/.config/x11/keymap_cursed.xkb "$DISPLAY"
+elif [ -f ~/.config/x11/keymap_cursed.xkb ]; then
+  xkbcomp -w2 ~/.config/x11/keymap_cursed.xkb "$DISPLAY"
 elif [ -f ~/.config/x11/keymap.xkb ]; then
   xkbcomp -w2 ~/.config/x11/keymap.xkb "$DISPLAY"
 else
