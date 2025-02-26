24d7bebd822080ba214bb9dfe1d36e626ebd0f37
Cortan122 <Cortan122@gmail.com>
2025-02-26 16:01:36 +0100

    sublime text dictionary

--- configure_firefox.sh
+++ configure_firefox.sh
@@ -3,10 +3,22 @@
 set -e
 cd -- "$(dirname -- "$0")"
 
-# perl -ne 'print unless $seen{$_}++' .config/firefox-chrome/persdict.dat | sponge .config/firefox-chrome/persdict.dat
-
 profile="$HOME/.mozilla/firefox/$(awk -F= '/Default/ {print $2;exit}' ~/.mozilla/firefox/profiles.ini)/"
 cp -vu .config/firefox-chrome/*.{css,png,jpeg} "$profile/chrome"
 cp -vu .config/firefox-chrome/user.js "$profile/user.js"
+
+# this script is also responsible for handling the personal dictionaries
+presdict=.config/firefox-chrome/persdict.dat
+if [ "$profile/persdict.dat" -ot "$presdict" ]; then
+  perl -ne 'print unless $seen{$_}++' "$presdict" | sponge "$presdict"
+fi
+
 cp -vu .config/firefox-chrome/persdict.dat "$profile/persdict.dat"
 cp -vu .config/firefox-chrome/persdict.dat ~/.local/share/TelegramDesktop/tdata/dictionaries/custom
+
+# converting the dict for sublime text
+presdict_subl=~/.config/sublime-text/Packages/Persdict/Preferences.sublime-settings
+if [ "$presdict_subl" -ot "$presdict" ]; then
+  jq -Rs '{added_words: [.|split("\n")|.[]|select(length > 0)]}' "$presdict" \
+    | install -Dv /dev/stdin "$presdict_subl"
+fi
