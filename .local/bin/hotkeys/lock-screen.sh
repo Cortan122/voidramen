273be3bcd236a48ecda9bf488d6a93e5953fa0b8
Cortan122 <Cortan122@gmail.com>
2026-06-04 20:26:00 +0200

    notifications toggle

--- .local/bin/hotkeys/lock-screen.sh
+++ .local/bin/hotkeys/lock-screen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 # todo: force it to redraw itself
-i3lock -i ~/.config/wall.png --show-keyboard-layout "$@"
+dunstctl set-paused true
+i3lock -i ~/.config/wall.png --show-keyboard-layout --nofork "$@"
+dunstctl set-paused false
+
+pkill --signal SIGRTMIN+12 i3blocks
